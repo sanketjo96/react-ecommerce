4e691ad6a81fcb21691b705005704990edf1842b
Sanket <sanketjoshi96@gmail.com>
2022-06-12 16:58:29 +0530

    clkeanup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Cart from './Pages/Cart/Cart';
 import Home from './Pages/Home/Home';
+import ButtonAppBar from './Components/AppBar';
 
 function App() {
   return (
     <div className="App">
+      <ButtonAppBar></ButtonAppBar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
